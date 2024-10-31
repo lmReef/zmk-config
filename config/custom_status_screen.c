fee60e708d86c3e7d372d7e06739f75bb01b4e33
lmReef <ReefMatson@gmail.com>
2024-11-01 12:16:58 +1300

    basically default custom_status_screen

--- config/custom_status_screen.c
+++ config/custom_status_screen.c
@@ -1,4 +1,4 @@
-/* #include <zmk/display/status_screen.h> */
+#include <custom_status_screen.h>
 #include <zmk/display/widgets/battery_status.h>
 #include <zmk/display/widgets/layer_status.h>
 #include <zmk/display/widgets/output_status.h>
@@ -37,33 +37,30 @@ lv_obj_t *zmk_display_status_screen() {
                LV_ALIGN_TOP_RIGHT, 0, 0);
 #endif
 
-  /* #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS) */
-  /*   zmk_widget_output_status_init(&output_status_widget, screen); */
-  /*   lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), */
-  /*                LV_ALIGN_TOP_LEFT, 0, 0); */
-  /* #endif */
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+  zmk_widget_output_status_init(&output_status_widget, screen);
+  lv_obj_align(zmk_widget_output_status_obj(&output_status_widget),
+               LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
 
-  /* #if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS) */
-  /*   zmk_widget_peripheral_status_init(&peripheral_status_widget, screen); */
-  /*   lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget),
-   */
-  /*                LV_ALIGN_TOP_LEFT, 0, 0); */
-  /* #endif */
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+  zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+  lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget),
+               LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
 
-  /* #if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS) */
-  /*   zmk_widget_layer_status_init(&layer_status_widget, screen); */
-  /*   lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
-   */
-  /*                              lv_theme_get_font_small(screen),
-   * LV_PART_MAIN); */
-  /*   lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), */
-  /*                LV_ALIGN_BOTTOM_LEFT, 0, 0); */
-  /* #endif */
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
+  zmk_widget_layer_status_init(&layer_status_widget, screen);
+  lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
+                             lv_theme_get_font_small(screen), LV_PART_MAIN);
+  lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget),
+               LV_ALIGN_BOTTOM_LEFT, 0, 0);
+#endif
 
-  /* #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS) */
-  /*   zmk_widget_wpm_status_init(&wpm_status_widget, screen); */
-  /*   lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), */
-  /*                LV_ALIGN_BOTTOM_RIGHT, 0, 0); */
-  /* #endif */
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+  zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+  lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget),
+               LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+#endif
   return screen;
 }
